851a4f5929296f41e77dd41fbb7db33baaa81def
FICproject <dvd_tudor@yahoo.com>
2017-01-10 13:31:08 +0200

    Update video2.cpp
    adding socket

--- video2.cpp
+++ video2.cpp
@@ -206,7 +206,9 @@ int main(int argc, char* argv[])
 	//program
 	bool trackObjects = true;
 	bool useMorphOps = true;
-
+	int sockfd, portno, n;
+    	struct sockaddr_in serv_addr;
+    	struct hostent *server;
 	Point p;
 	//Matrix to store each frame of the webcam feed
 	Mat cameraFeed;
@@ -228,7 +230,25 @@ int main(int argc, char* argv[])
 	//start an infinite loop where webcam feed is copied to cameraFeed matrix
 	//all of our operations will be performed within this loop
 
-
+	portno = 20231;
+      	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        server = gethostbyname("193.226.12.217");
+        bzero((char *) &serv_addr, sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET;
+   	bcopy((char *)server->h_addr,(char *)&serv_addr.sin_addr.s_addr,server->h_length);
+   	serv_addr.sin_port = htons(portno);
+    
+ 	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        	printf("ERROR connecting");
+    	n = write(sockfd,"f\n",1);
+    	sleep(2);
+        n = write(sockfd,"s\n",1);
+        sleep(2);
+        n = write(sockfd,"l\n",1);
+        sleep(1);
+        n = write(sockfd,"s\n",1);
+        if (n < 0) 
+        	printf("ERROR writing to socket");
 
 	
 	while (1) {
